ae36277a83e44a55f31ccb95fb8b649667182b62
Irsyad Abdul <abdulirsyad15@gmail.com>
2024-11-12 08:32:29 +0700

    feat: add hover mark

--- src/components/tile/tile.component.tsx
+++ src/components/tile/tile.component.tsx
@@ -5,7 +5,7 @@ import styles from './tile.module.css';
 import { CircleMark } from '../marks/circle.component';
 import { TimesMark } from '../marks/times.component';
 import { checkWinCondition } from '@/utils/check.util';
-import { useMemo } from 'react';
+import { useCallback, useMemo } from 'react';
 
 export const Tile = ({ tile }: { tile: ITile }) => {
   const {
@@ -31,38 +31,44 @@ export const Tile = ({ tile }: { tile: ITile }) => {
     [winner, winningTiles, tile.coordinate],
   );
 
+  const handleClick = useCallback(() => {
+    if (!tile.mark) {
+      const newTiles = tiles?.map((item) => {
+        return item.coordinate.x === tile.coordinate.x &&
+          item.coordinate.y === tile.coordinate.y
+          ? { ...item, mark: turn }
+          : item;
+      });
+      setTiles(newTiles);
+      const { winCondition, tiles: winningTiles } = checkWinCondition(
+        newTiles!,
+        turn,
+      );
+      if (winCondition) {
+        setWinner(turn);
+        setWinningTiles(winningTiles!);
+      } else changeTurn();
+    }
+  }, [changeTurn, setTiles, setWinner, setWinningTiles, tile, tiles, turn]);
+
   return (
     <div
       className={`${styles.tile} ${tile.mark ? styles.marked : styles.unmarked} ${isWinningTiles !== undefined ? (isWinningTiles ? styles['winning-tile'] : styles['not-winning-tile']) : ''}`}
-      onClick={
-        winner
-          ? undefined
-          : () => {
-              if (!tile.mark) {
-                const newTiles = tiles?.map((item) => {
-                  return item.coordinate.x === tile.coordinate.x &&
-                    item.coordinate.y === tile.coordinate.y
-                    ? { ...item, mark: turn }
-                    : item;
-                });
-                setTiles(newTiles);
-                const { winCondition, tiles: winningTiles } = checkWinCondition(
-                  newTiles!,
-                  turn,
-                );
-                if (winCondition) {
-                  setWinner(turn);
-                  setWinningTiles(winningTiles!);
-                } else changeTurn();
-              }
-            }
-      }
+      onClick={winner ? undefined : handleClick}
     >
       {tile.mark === 'x' ? (
         <TimesMark style={{ fontSize: '3.5rem' }} />
       ) : tile.mark === 'o' ? (
         <CircleMark style={{ fontSize: '3.5rem' }} />
-      ) : null}
+      ) : (
+        <div className={styles['hover-mark']}>
+          {turn === 'x' ? (
+            <TimesMark style={{ fontSize: '3.5rem' }} />
+          ) : (
+            <CircleMark style={{ fontSize: '3.5rem' }} />
+          )}
+        </div>
+      )}
     </div>
   );
 };
